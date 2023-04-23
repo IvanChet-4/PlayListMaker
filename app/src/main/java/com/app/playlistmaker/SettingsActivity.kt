e14566f623fb479752bfecf40edae2411d2b1253
IvanChet-4 <github_isc@list.ru>
2023-04-23 19:50:47 +0300

    Update SettingsActivity.kt; Add function open website.

--- app/src/main/java/com/app/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/app/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.app.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import android.widget.Button
 
 class SettingsActivity : AppCompatActivity() {
@@ -12,9 +14,19 @@ class SettingsActivity : AppCompatActivity() {
 
         val settingsHome = findViewById<Button>(R.id.button_settings_Home)
 
+        val settingsConfirm = findViewById<Button>(R.id.button_settings_Confirm)
+
         settingsHome.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+            finish()
+        }
+
+        settingsConfirm.setOnClickListener {
+            val urlConfirm = "https://yandex.ru/legal/practicum_offer/"
+            val openSite = Intent(Intent.ACTION_VIEW);
+            openSite.setData(Uri.parse(urlConfirm));
+            startActivity(openSite);
             finish()
         }
     }
