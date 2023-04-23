2b58eb77978030b7a5f01715a51b3bed5c43a7cf
IvanChet-4 <github_isc@list.ru>
2023-04-23 20:15:34 +0300

    Update SettingsActivity.kt; Add function share, mailto.

--- app/src/main/java/com/app/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/app/playlistmaker/SettingsActivity.kt
@@ -14,11 +14,38 @@ class SettingsActivity : AppCompatActivity() {
 
         val settingsHome = findViewById<Button>(R.id.button_settings_Home)
 
+        val shareToOtherApp = findViewById<Button>(R.id.button_settings_Share)
+
+        val writeToSupport = findViewById<Button>(R.id.button_settings_Support)
+
         val settingsConfirm = findViewById<Button>(R.id.button_settings_Confirm)
+
+
 
         settingsHome.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+            finish()
+        }
+
+        shareToOtherApp.setOnClickListener {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Привет! Вот ссылка на мое приложение: https://iv-4.ru/login")
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+            finish()
+        }
+
+        writeToSupport.setOnClickListener {
+            val message = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("isc47@bk.ru"))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
             finish()
         }
 
