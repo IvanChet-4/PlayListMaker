ecae1a98e8cc00e0cf3059c821076dd3f70e221f
IvanChet-4 <github_isc@list.ru>
2023-04-27 00:55:44 +0300

    Update Project;

--- app/src/main/java/com/app/playlistmaker/FinderActivity.kt
+++ app/src/main/java/com/app/playlistmaker/FinderActivity.kt
@@ -3,85 +3,72 @@ package com.app.playlistmaker
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.EditText
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
-import androidx.annotation.RequiresApi
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
+import android.widget.ImageView
 
 class FinderActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_finder)
+       private val KEY_TEXT = ""
+        @SuppressLint("MissingInflatedId")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_finder)
 
+            val inputEditText = findViewById<EditText>(R.id.inputEditText)
+            val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
-        val linearLayout = findViewById<LinearLayout>(R.id.container)
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
-        val clearButton = findViewById<ImageView>(R.id.clearIcon)
-
-        val finderToHome = findViewById<Button>(R.id.button_finder_Home)
-        finderToHome.setOnClickListener {
-            val displayIntent = Intent(this, MainActivity::class.java)
-            startActivity(displayIntent)
-            finish()
-        }
-
-
-
-
-
-
-        clearButton.setOnClickListener {
-            inputEditText.setText("")
-        }
-
-        val simpleTextWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
+            clearButton.setOnClickListener {
+                inputEditText.setText("")
+                val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                keyboard.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+                inputEditText.clearFocus()
             }
 
-            @RequiresApi(Build.VERSION_CODES.M)
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.isNullOrEmpty()) {
-                    linearLayout.setBackgroundColor(getColor(R.color.prime_neutral))
-                } else {
-                    val input = s.toString()
-                    if (isPrime(input.toInt())) {
-                        linearLayout.setBackgroundColor(getColor(R.color.purple_700))
-                    } else {
-                        linearLayout.setBackgroundColor(getColor(R.color.purple_200))
-                    }
+
+            val simpleTextWatcher = object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int){
+                    // empty
                 }
-                clearButton.visibility = clearButtonVisibility(s)
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    clearButton.visibility = clearButtonVisibility(s)
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    // empty
+                }
             }
-
-            override fun afterTextChanged(s: Editable?) {
-                // empty
-            }
-        }
-        inputEditText.addTextChangedListener(simpleTextWatcher)
-    }
-
-    private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
-        } else {
-            View.VISIBLE
-        }
-    }
-
-    private fun isPrime(number: Int): Boolean {
-        for (i in 2..number / 2) {
-            if (number % i == 0) {
-                return false
+            inputEditText.addTextChangedListener(simpleTextWatcher)
+            val arrowButton = findViewById<Button>(R.id.button_finder_Home)
+            arrowButton.setOnClickListener {
+                val displayIntent = Intent(this, MainActivity::class.java)
+                startActivity(displayIntent)
+                finish()
             }
         }
-        return true
+
+        override fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            val inputEditText = findViewById<EditText>(R.id.inputEditText)
+            outState.putString(KEY_TEXT, inputEditText.text.toString())
+        }
+
+        override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+            super.onRestoreInstanceState(savedInstanceState)
+            savedInstanceState.getString(KEY_TEXT, "")
+        }
+
+        private fun clearButtonVisibility(s: CharSequence?): Int {
+            return if (s.isNullOrEmpty()) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
     }
-}
